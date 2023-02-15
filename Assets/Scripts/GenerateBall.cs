cae33656ff815517da25c9a52af7cc781c792003
flaars <fcateloiu08@gmail.com>
2023-02-15 19:19:02 +0100

    Hola

---
+++ Assets/Scripts/GenerateBall.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBall : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    public void SpawnIt()
+    {
+        Debug.Log("Has clicado.");
+    }
+}
